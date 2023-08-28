5c5840d1d4fcff3890265a8f1b0f1fc1f3b5b8fd
Vural Sayan <vuralsayan01@gmail.com>
2023-08-29 00:11:46 +0300

    Kullanıcı password şifreleme işlemi tamamlandı

--- RSAMessageApp/Kayıt.Designer.cs
+++ RSAMessageApp/Kayıt.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnRegister = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -44,19 +44,19 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Kullanıcı Adı:";
             // 
-            // textBox1
+            // TxtUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 52);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 30);
-            this.textBox1.TabIndex = 1;
+            this.TxtUsername.Location = new System.Drawing.Point(179, 52);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(260, 30);
+            this.TxtUsername.TabIndex = 1;
             // 
-            // textBox2
+            // TxtPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(179, 97);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(260, 30);
-            this.textBox2.TabIndex = 3;
+            this.TxtPassword.Location = new System.Drawing.Point(179, 97);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(260, 30);
+            this.TxtPassword.TabIndex = 3;
             // 
             // label2
             // 
@@ -67,14 +67,15 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Şifre:";
             // 
-            // button1
+            // BtnRegister
             // 
-            this.button1.Location = new System.Drawing.Point(289, 145);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 43);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Kayıt Ol";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnRegister.Location = new System.Drawing.Point(289, 145);
+            this.BtnRegister.Name = "BtnRegister";
+            this.BtnRegister.Size = new System.Drawing.Size(150, 43);
+            this.BtnRegister.TabIndex = 4;
+            this.BtnRegister.Text = "Kayıt Ol";
+            this.BtnRegister.UseVisualStyleBackColor = true;
+            this.BtnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
             // 
             // Kayıt
             // 
@@ -82,13 +83,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(194)))), ((int)(((byte)(208)))));
             this.ClientSize = new System.Drawing.Size(518, 254);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.BtnRegister);
+            this.Controls.Add(this.TxtPassword);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtUsername);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Kayıt";
@@ -102,9 +103,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.TextBox TxtPassword;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnRegister;
     }
 }
--- RSAMessageApp/Kayıt.cs
+++ RSAMessageApp/Kayıt.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient; // SQL Server için gerekli kütüphane
+using System.Security.Cryptography; // RSA için gerekli kütüphane
 
 namespace RSAMessageApp
 {
@@ -16,5 +18,67 @@ namespace RSAMessageApp
         {
             InitializeComponent();
         }
+
+        // SQL Server bağlantısı 
+        SqlConnection baglanti = new SqlConnection(@"Data Source=Vural\SQLEXPRESS;Initial Catalog=DbRsaMessage;Integrated Security=True");
+
+
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {
+            RegisterUser(TxtUsername.Text, TxtPassword.Text);    
+
+
+            //Kullanıcı şifresini SHA-256 ile hashleyen fonksiyon
+            void RegisterUser(string username, string password)
+            {
+                byte[] hashedPassword = HashPassword(password);
+
+                string hashedPasswordString = Convert.ToBase64String(hashedPassword);
+
+                MessageBox.Show(hashedPasswordString);
+
+                //Veritabanına kayıt işlemi 
+                SaveUserToDatabase(username, hashedPasswordString);
+            }
+
+
+            byte[] HashPassword(string password)
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    //Girilen şifreyi byte dizisine çevirme
+                    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                    return sha256.ComputeHash(passwordBytes);
+                }
+            }
+
+
+            //Kullanıcıyı veritabanına kaydetme
+            void SaveUserToDatabase(string username, string hashedPassword)
+            {
+                string connectionString = @"Data Source=Vural\SQLEXPRESS;Initial Catalog=DbRsaMessage;Integrated Security=True"; // Veritabanı bağlantı dizesi
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO TBLUSERS (Username, PasswordHash) VALUES (@Username, @HashedPassword)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@HashedPassword", hashedPassword);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            MessageBox.Show("Kayıt Başarılı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);   
+
+
+        }
     }
 }
+
+
+           
